ce7808cff75882404c5a38cc4c9f078a3d614073
LAPTOP-46S041F2\권규현 <kyuhyun2920@naver.com>
2021-04-05 16:25:55 +0900

    change4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import './App.css';
-import Post from '../components/Post';
 
-function App() {
-  return (
-    <Route path="/post" exact component={Post}/> 
-=======
+
+
 import React from 'react'
 import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
@@ -19,7 +14,7 @@ function App() {
         <Route path="/" exact component={Login} />
       </BrowserRouter>
     </React.Fragment>
->>>>>>> cf6c2045900206b1e1a2906887c553fc386277cd
+
   );
 }
 
