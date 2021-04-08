0ca702a183477c07e5ba9dbe99e6c57233d5ff42
LAPTOP-46S041F2\권규현 <kyuhyun2920@naver.com>
2021-04-08 14:56:26 +0900

    mydang

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -12,24 +12,18 @@ const Write = (props) => {
   const title_ref = React.useRef("안녕");
   const contents_ref = React.useRef("하이");
   const image_ref = React.useRef("그래");
-  
-  
+  const price_ref = React.useRef("가격");
  
-  
   const Create =()=>{
-    
-    
 
     const form = new FormData();
     form.append('title',title_ref.current.value);
     form.append('contents',contents_ref.current.value);
     form.append('files',image_ref.current.files[0]);
-      // console.log(title_ref.current.value)
-      // console.log(contents_ref.current.value)
-      // console.log(image_ref.current.files[0].name)
-      // for(var key of form.keys()){
-      //   console.log(key);
-      // }
+    form.append('price', price_ref.current.value);
+      for(var key of form.keys()){
+        console.log(key);
+      }
      
       
      axios(
@@ -42,75 +36,61 @@ const Write = (props) => {
         url:"http://15.165.77.77:8080/api/boards",
         data: form
       }
-     )
-
-    
-  
+      )
       .then((response)=>{
-        console.log(response);
-
-        
+        console.log(response);  
       }).catch(error=>{
         console.log(error);
       })
-
       props.history.push("/postlist")
-    }
-
-   
-    
+  }
 
   return (
     <React.Fragment>
       
       <Grid is_flex border_bottom="1px solid #e9ecef" padding="10px">
-      
       <pButton onClick={()=>props.history.push("/postlist")}>
-        <Text size="12px">닫기</Text></pButton>
-
-
-
+      <Text size="12px">닫기</Text>
+      </pButton>
       <Text bold size="20px">중고거래 글쓰기</Text>
-      
       <pButton onClick={()=>{Create()}}>
-      <Text size="12px">완료</Text></pButton>
+      <Text size="12px">완료</Text>
+      </pButton>
       </Grid>
       
       <Grid border_bottom="1px solid #e9ecef" padding="30px">
         <input type="file" ref={image_ref}/>
       </Grid>
+
       <Grid border_bottom="1px solid #e9ecef" padding="20px">
        <NLInput placeholder="글 제목"  width="200px" ref={title_ref}></NLInput>
       </Grid>
+
       <Grid is_flex border_bottom="1px solid #e9ecef" padding="20px">
        <Text>카테고리 선택</Text>
        <pButton>></pButton>
       </Grid>
+
       <Grid is_flex border_bottom="1px solid #e9ecef" padding="20px">
-       <NLInput placeholder="가격입력(선택사항)" width="200px"></NLInput>
+       <NLInput ref={price_ref} placeholder="가격입력(선택사항)" width="200px"></NLInput>
        <Text><input type="checkbox"></input>가격제안받기</Text>
       </Grid>
+
       <Grid is_flex border_bottom="1px solid #e9ecef" padding="60px">
        <NLInput type="text" ref={contents_ref} width="800px" height="100px"
         placeholder="올릴 게시글 내용을 작성해주세요">
-
        </NLInput>
       </Grid>
-    
 
-     
     </React.Fragment>
   );
 };
 
-
 const NLInput = styled.input`
-  
   outline: none;
   width: ${(props)=>props.width};
   height: ${(props)=>props.height};
   border: none;
-  
 `;
 
 
